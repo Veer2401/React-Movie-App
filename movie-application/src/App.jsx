f3ab8e24fcbfd7ebc8aa484ad0e3138815b7b6fc
Veer Harischandrakar <veerharischandrakar@gmail.com>
2025-08-17 11:55:26 +0530

    UseState{} hook Basics
    Hooks are special functions in REACT

--- movie-application/src/App.jsx
+++ movie-application/src/App.jsx
@@ -4,16 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
   return(
     <div className='card'>
     <h2>{title}</h2>
+
+    <button onClick={() => setHasLiked(!hasLiked)}>
+      {hasLiked ? '♥️' : '🤍'}
+    </button>
     </div>
   )
 }
 
 const App = () => {
+
   return(
     <div className='card-container'>
+      <h2>Movie Application</h2>
       <Card title="Star Wars" />
       <Card title="The Godfather" />
       <Card title="Inception" />
