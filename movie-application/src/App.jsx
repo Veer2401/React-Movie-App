f1a0b57cd5dca7cd8df1a31e67666083fa606328
Veer Harischandrakar <veerharischandrakar@gmail.com>
2025-08-17 12:24:42 +0530

    Starting to build the movie app now
    Used the 'rafce' snippet and now initialising Tailwind CSS setup

--- movie-application/src/App.jsx
+++ movie-application/src/App.jsx
@@ -1,44 +1,10 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-const Card = ({title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`Card with title "${title}" has been ${hasLiked ? 'liked' : 'unliked'}.`);
-  }, [hasLiked]);
-
-  useEffect(() => {
-   console.log('Card Rendered') 
-  },[]);
-
-  return(
-    <div className='card' onClick={() => setCount((prevState) => prevState + 1)}>
-    <h2>{title} - {count ? count : null}</h2>
-
-    <button onClick={() => setHasLiked(!hasLiked)}>
-      {hasLiked ? '♥️' : '🤍'}
-    </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-
-  return(
-    <div className='card-container'>
-      <h2>Movie Application</h2>
-      <Card title="Star Wars" />
-      <Card title="The Godfather" />
-      <Card title="Inception" />
-      <Card title="The Dark Knight" />
-      <Card title="Pulp Fiction" />
-      
+  return (
+    <div>
+      <h2>App</h2>
     </div>
-   
   )
 }
 
