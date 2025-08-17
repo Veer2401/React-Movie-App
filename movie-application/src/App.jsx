a494d55c7dfd9fe83ca554fe7b4fe5363b807064
Veer Harischandrakar <veerharischandrakar@gmail.com>
2025-08-17 10:24:01 +0530

    Practising

--- movie-application/src/App.jsx
+++ movie-application/src/App.jsx
@@ -3,9 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Card = () => {
+  return(
+    <h2>Card Component</h2>
+  )
+}
+
 const App = () => {
   return(
-    <h2>Functional Arrow Component</h2>
+    <div>
+      <h2>Movie App</h2>
+      <Card />
+    </div>
+   
   )
 }
 
