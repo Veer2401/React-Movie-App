95e03aedc866568afc9a08f8440b1b1074903e77
Veer Harischandrakar <veerharischandrakar@gmail.com>
2025-08-18 11:10:20 +0530

    Update App.jsx

--- movie-application/src/App.jsx
+++ movie-application/src/App.jsx
@@ -82,7 +82,7 @@ const App = () => {
            {/* <img src="./logo.png" alt="Hero-Banner" className="w-[400px] h-[300px]" /> */}
             <img src="./hero.png" alt="Hero-Banner" className="w-[800px] h-auto" />
             <br></br>
-            <h1>Cinematic <span className='text-gradient'>X</span></h1>
+            <h1>Cinematic<span className='text-gradient'>X</span></h1>
             <h1>Your <span className="text-gradient">movie </span> dictionary 🎬</h1>
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           <br>
